1960f26ff7b79e6e3f28c4e674c738c6e7409fbe
unknown <YY231@ccs.local>
2025-03-26 20:52:09 +0800

    GetADD functionality

--- routes/exams.js
+++ routes/exams.js
@@ -2,3 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = router;
+
+
+
+router.get('/', (req, res) => {
+    const exams = [
+        { id: 1, name: "Perry Ian Mendoza" },
+      { id: 2, name: "Adonis Espinosa" },
+      { id: 3, name: "Cyrus Joven" },
+      { id: 4, name: "Pearl Marie Monceda" }
+    ];
+    res.json(exams);
+  });
